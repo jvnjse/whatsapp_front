e0703b942f43e30e90a5f7aa0a4c148b3a4117e1
jvnjse <84738604+jvnjse@users.noreply.github.com>
2024-01-09 22:14:39 +0530

    feature enabling

--- whatsapp_react/src/Admin/User/Users.jsx
+++ whatsapp_react/src/Admin/User/Users.jsx
@@ -25,7 +25,7 @@ function Users() {
     const Getusers = () => {
         axios.get(`${config.baseUrl}users/`, { headers: headers })
             .then((response) => {
-                setuserdata(response.data)
+                setuserdata(response.data.staff_users)
                 // console.log(response.data)
             })
             .catch((error) => {
@@ -58,7 +58,7 @@ function Users() {
 
 
 
-    const UserDetails = ({ userid }) => {
+    const UserDetails = ({ userid, useremail }) => {
 
         const headers = {
             'Content-Type': 'application/json',
@@ -70,88 +70,106 @@ function Users() {
                 [feature]: !prevState[feature]
             }))
         };
-        useEffect(() => {
-            const updateUserFeatures = async () => {
-                try {
-                    const response = await axios.put(`${config.baseUrl}users/${userid}/`, userfeatures, { headers: headers });
-                    // Update the state with the response data
-                    setUserfeatures(response.data);
+        const updateUserFeatures = () => {
+            axios.put(`${config.baseUrl}users/${userid}/`, userfeatures, { headers: headers })
+                .then((response) => {
+                    // setUserfeatures(response.data);
                     console.log(response.data, "ddd");
-                } catch (error) {
+                })
+                .catch((error) => {
                     console.log(error);
-                }
-            };
-
-            // Call the API only when userfeatures change
-            if (userfeaturesChanged(userfeatures, updateUserFeatures)) {
-                updateUserFeatures();
-            }
-        }, [userfeatures, config.baseUrl, userid, headers]);
-
-        const userfeaturesChanged = (newUserFeatures, callback) => {
-            // Compare the new and previous userfeatures values
-            const userfeaturesChanged = JSON.stringify(newUserFeatures) !== JSON.stringify(userfeatures);
-
-            // If changed, execute the callback
-            if (userfeaturesChanged && typeof callback === 'function') {
-                return true;
-            }
-
-            return false;
+                })
         };
+        useEffect(() => {
+            updateUserFeatures()
+
+
+        }, [userfeatures]);
+
+
         return (
             <div className='absolute w-screen h-screen top-0 left-0 bg-black/10 flex justify-center items-center '
                 onClick={() => { setUserpopup(false); }} >
                 <div className='bg-white w-10/12 h-[80%] rounded-lg p-4' onClick={(event) => { event.stopPropagation(); }}>
                     <div className='text-xl font-bold'>User Details</div>
                     {userdetail && <div className='bg-white p-2 rounded-sm flex flex-col'>
-                        <div className='font-semibold'>{userdetail.email}</div>
-                        <label htmlFor="toggleExcel" className="flex items-center cursor-pointer">
-                            <span>Excel Feature</span>
+                        {/* <div className='font-semibold text-3xl py-4'>{useremail}</div> */}
+                        <label htmlFor="toggleExcel" className="flex justify-between items-center cursor-pointer  p-3 rounded-xl">
+                            <div className='font-semibold text-3xl py-4'>{useremail}</div>
                             <div className="relative">
                                 <input
                                     type="checkbox"
                                     id="toggleExcel"
                                     className="sr-only"
-                                    checked={userfeatures.excel_feature}
-                                    onChange={() => handleFeatureToggle('excel_feature')}
+                                    checked={userfeatures.is_active}
+                                    onChange={() => handleFeatureToggle('is_active')}
                                 />
-                                <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                                <div className="block bg-gray-500 w-14 h-8 rounded-full back-check"></div>
                                 <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
                             </div>
                         </label>
+                        <div className='flex flex-wrap gap-4'>
 
-                        <label htmlFor="toggleImage" className="flex items-center cursor-pointer">
-                            <span>Image Feature</span>
-                            <div className="relative">
-                                <input
-                                    type="checkbox"
-                                    id="toggleImage"
-                                    className="sr-only"
-                                    checked={userfeatures.image_feature}
-                                    onChange={() => handleFeatureToggle('image_feature')}
-                                />
-                                <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
-                                <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-                            </div>
-                        </label>
+                            <label htmlFor="toggleExcel" className="flex items-center cursor-pointer bg-slate-200 max-w-max p-3 rounded-xl">
+                                <span>Excel Feature</span>
+                                <div className="relative">
+                                    <input
+                                        type="checkbox"
+                                        id="toggleExcel"
+                                        className="sr-only"
+                                        checked={userfeatures.excel_feature}
+                                        onChange={() => handleFeatureToggle('excel_feature')}
+                                    />
+                                    <div className="block bg-gray-500 w-14 h-8 rounded-full back-check"></div>
+                                    <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                                </div>
+                            </label>
 
-                        <label htmlFor="toggleMessaging" className="flex items-center cursor-pointer">
-                            <span>Messaging Feature</span>
-                            <div className="relative">
-                                <input
-                                    type="checkbox"
-                                    id="toggleMessaging"
-                                    className="sr-only"
-                                    checked={userfeatures.messaging_feature}
-                                    onChange={() => handleFeatureToggle('messaging_feature')}
-                                />
-                                <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
-                                <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-                            </div>
-                        </label>
+                            <label htmlFor="toggleImage" className="flex items-center cursor-pointer bg-slate-200 max-w-max p-3 rounded-xl">
+                                <span>Image Feature</span>
+                                <div className="relative">
+                                    <input
+                                        type="checkbox"
+                                        id="toggleImage"
+                                        className="sr-only"
+                                        checked={userfeatures.image_feature}
+                                        onChange={() => handleFeatureToggle('image_feature')}
+                                    />
+                                    <div className="block bg-gray-500 w-14 h-8 rounded-full back-check"></div>
+                                    <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                                </div>
+                            </label>
 
-                        <br />
+                            <label htmlFor="toggleMessaging" className="flex items-center cursor-pointer bg-slate-200 max-w-max p-3 rounded-xl">
+                                <span>Messaging Feature</span>
+                                <div className="relative">
+                                    <input
+                                        type="checkbox"
+                                        id="toggleMessaging"
+                                        className="sr-only"
+                                        checked={userfeatures.messaging_feature}
+                                        onChange={() => handleFeatureToggle('messaging_feature')}
+                                    />
+                                    <div className="block bg-gray-500 w-14 h-8 rounded-full back-check"></div>
+                                    <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                                </div>
+                            </label>
+                            <label htmlFor="togglePersonal" className="flex items-center cursor-pointer bg-slate-200 max-w-max p-3 rounded-xl">
+                                <span>Personalised Messaging Feature</span>
+                                <div className="relative">
+                                    <input
+                                        type="checkbox"
+                                        id="togglePersonal"
+                                        className="sr-only"
+                                        checked={userfeatures.personalised_feature}
+                                        onChange={() => handleFeatureToggle('personalised_feature')}
+                                    />
+                                    <div className="block bg-gray-500 w-14 h-8 rounded-full back-check"></div>
+                                    <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+                                </div>
+                            </label>
+                        </div>
+
                     </div>}
                 </div>
 
@@ -178,7 +196,7 @@ function Users() {
                                     >more details</div>
                                 </div>
                                 {userpopup && <div>
-                                    <UserDetails userid={user.id} />
+                                    <UserDetails userid={user.id} useremail={user.email} />
 
                                 </div>
                                 }
--- whatsapp_react/src/Landing.jsx
+++ whatsapp_react/src/Landing.jsx
@@ -25,66 +25,58 @@ function Landing({ accessToken, isvalid }) {
 
     return (
         <div className=' w-screen h-[100%] overflow-hidden '>
-            {/* nav */}
-            <div className='bg-[#083929] text-[#f0f0f0] flex justify-between px-16 py-4'>
+            <div className='bg-[#083929] text-[#f0f0f0] flex justify-between px-16 py-4 max-sm:px-2 max-sm:text-sm'>
                 <div className=''>logo</div>
-                <div className='flex gap-6 text-sm font-medium'>
+                <div className='flex gap-6 text-sm font-medium max-sm:text-xs max-sm:gap-0' >
                     <button className='p-2 hover:text-[#1a4735] hover:bg-[#eaeeec] rounded-lg'><Link to='#contact'>Contact Us</Link></button>
                     <button className='p-2 hover:text-[#1a4735] hover:bg-[#eaeeec] rounded-lg'>{accessToken && isvalid ? <Link to={is_staff ? '/admin/messages' : '/messages'}>Go to Module</Link> : <Link to='/login'>Login  |  SignUp</Link>}</button>
                 </div>
             </div>
-            {/* nav */}
 
-            {/* <motion.div style={{
-                scale
-            }}
-                className='flex justify-around relative  flex-wrap-reverse animate-section1'>
-                xxx
-            </motion.div> */}
             <section className='flex flex-col gap-3 w-full text-center bg-[#f0f0f0]  text-[#000] '>
-                <div className=' font-bold text-5xl p-14 px-20 leading-snug'>Introducing Alt WhatsApp <br></br> Your Ultimate WhatsApp Marketing Companion!</div>
-                <div>Get started now for enhanced outreach and engagement.</div>
-                <div className='flex gap-16 justify-center text-[#f0f0f0] py-10'>
-                    <button className='bg-[#2f2e2e] px-5 py-2 rounded-full hover:bg-[#f0f0f0] hover:text-[#2f2e2e] hover:border border border-[#2f2e2e]'>Try Demo</button>
-                    <button className='bg-[#2f2e2e] px-5 py-2 rounded-full hover:bg-[#f0f0f0] hover:text-[#2f2e2e] hover:border border border-[#2f2e2e]' ><Link to='/login'>Get Started</Link></button>
+                <div className=' font-bold text-5xl p-14 px-20 leading-snug max-md:p-1 max-md:text-3xl max-sm:text-lg'>Introducing Alt WhatsApp <br></br> Your Ultimate WhatsApp Marketing Companion!</div>
+                <div className='max-sm:text-xs'>Get started now for enhanced outreach and engagement.</div>
+                <div className='flex gap-16 justify-center text-[#f0f0f0] py-10 max-sm:gap-4'>
+                    <button className='bg-[#2f2e2e] px-5 py-2 rounded-full hover:bg-[#f0f0f0] hover:text-[#2f2e2e] hover:border border border-[#2f2e2e] max-sm:p-2 max-sm:text-xs'>Try Demo</button>
+                    <button className='bg-[#2f2e2e] px-5 py-2 rounded-full hover:bg-[#f0f0f0] hover:text-[#2f2e2e] hover:border border border-[#2f2e2e] max-sm:p-2 max-sm:text-xs' ><Link to='/login'>Get Started</Link></button>
                 </div>
             </section>
-            <section className='flex justify-center items-center gap-7 w-full text-center bg-[#f0f0f0] py-10'>
-                <iframe width="849" height="434" src="https://www.youtube.com/embed/GmK43VVcsWc" title="How to Send Bulk WhatsApp Messages using the official WhatsApp Cloud APIs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <section className='flex justify-center items-center gap-7 w-full text-center bg-[#f0f0f0] py-10 max-sm:py-2'>
+                <iframe className='w-10/12 aspect-video' src="https://www.youtube.com/embed/GmK43VVcsWc" title="How to Send Bulk WhatsApp Messages using the official WhatsApp Cloud APIs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </section>
 
-            <section className='flex flex-col gap-20 w-full text-center bg-[#f0f0f0] text-[#000] '>
-                <div className=' font-extrabold text-3xl pt-20 px-20 leading-snug'>Why Alt WhatsApp?</div>
-                <div className='flex gap-32 justify-center px-28 '>
-                    <div className='max-w-sm  overflow-hidden rounded-2xl hover:shadow-2xl transition-shadow '>
-                        <img className='w-full' src="https://img.freepik.com/free-vector/businessmen-shaking-hands-through-display-video-call-smart-phone-internet-business-concept-cartoon-character-vector-illustration_1150-56245.jpg" alt="" />
+            <section className='flex flex-col gap-20 w-full text-center bg-[#f0f0f0] text-[#000] max-sm:gap-5 '>
+                <div className=' font-extrabold text-3xl pt-20 px-20 leading-snug max-sm:pt-3'>Why Alt WhatsApp?</div>
+                <div className='flex gap-32 justify-center px-28 max-sm:gap-3 max-sm:px-4 max-md:gap-6 max-md:px-6 '>
+                    <div className='w-5/12  overflow-hidden rounded-2xl transition-shadow '>
+                        <img className='w-full aspect-video hover:shadow-2xl' src="https://img.freepik.com/free-vector/businessmen-shaking-hands-through-display-video-call-smart-phone-internet-business-concept-cartoon-character-vector-illustration_1150-56245.jpg" alt="" />
                     </div>
-                    <div className='max-w-sm text-start flex flex-col gap-4'>
+                    <div className='w-5/12 text-start flex flex-col gap-4 max-sm:text-[8px]'>
                         <div>Advanced Automation Tools</div>
                         <div>Empower your WhatsApp marketing strategy with Alt WhatsApp's cutting-edge automation tools. Streamline messaging, scheduling, and customer interactions effortlessly, saving time and enhancing efficiency.</div>
                     </div>
                 </div>
-                <div className='flex gap-32 justify-center px-28 '>
-                    <div className='max-w-sm text-start flex flex-col gap-4'>
+                <div className='flex gap-32 justify-center px-28 max-sm:gap-3 max-sm:px-4  max-md:gap-6 max-md:px-6'>
+                    <div className='max-w-sm text-start flex flex-col gap-4 max-sm:text-[8px]'>
                         <div>Enhanced Analytics and Insights</div>
                         <div>Gain valuable insights into your WhatsApp marketing campaigns with Alt WhatsApp. Track performance metrics, analyze user engagement, and make data-driven decisions to optimize your outreach and maximize results.</div>
                     </div>
                     <div className='max-w-sm  overflow-hidden rounded-2xl hover:shadow-2xl transition-shadow '>
-                        <img className='w-full' src="https://img.freepik.com/free-vector/businessmen-shaking-hands-through-display-video-call-smart-phone-internet-business-concept-cartoon-character-vector-illustration_1150-56245.jpg" alt="" />
+                        <img className='w-full aspect-video' src="https://img.freepik.com/free-vector/businessmen-shaking-hands-through-display-video-call-smart-phone-internet-business-concept-cartoon-character-vector-illustration_1150-56245.jpg" alt="" />
                     </div>
                 </div>
             </section>
 
             <section id='contact' className='flex flex-col justify-center items-center gap-7 w-full text-center bg-[#afd5be] text-[#f0f0f0] py-10'>
                 <div className='flex gap-8 '>
-                    <div className='px-3 py-1  bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] '>MARKETING</div>
-                    <div className='px-3 py-1 bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] '>MARKETING</div>
-                    <div className='px-3 py-1 bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] '>MARKETING</div>
-                    <div className='px-3 py-1 bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] '>MARKETING</div>
+                    <div className='px-3 py-1  bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] max-sm:text-xs'>MARKETING</div>
+                    <div className='px-3 py-1 bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] max-sm:text-xs '>MARKETING</div>
+                    <div className='px-3 py-1 bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] max-sm:text-xs '>MARKETING</div>
+                    <div className='px-3 py-1 bg-[#282626] cursor-pointer hover:bg-[#f0f0f0] hover:text-[#282626] max-sm:text-xs '>MARKETING</div>
                 </div>
-                <div className='bg-[#fff] text-[#000] p-10 rounded-2xl flex flex-col gap-4'>
+                <div className='bg-[#fff] text-[#000] p-10 rounded-2xl flex flex-col gap-4 w-7/12 max-sm:w-10/12'>
                     <div>Talk to Us</div>
-                    <div className='flex gap-8'>
+                    <div className='flex gap-8 flex-wrap'>
                         <label className='flex flex-col text-left'>First Name<input type="text" className='input-border' /></label>
                         <label className='flex flex-col text-left'>Last Name<input type="text" className='input-border' /></label>
                     </div>
@@ -92,7 +84,7 @@ function Landing({ accessToken, isvalid }) {
                         {/* <label className='flex flex-col text-left'>First Name<input type="text" className='input-border' /></label> */}
                         <label className='flex flex-1 flex-col text-left'>Email<input type="email" className='input-border' /></label>
                     </div>
-                    <div className='flex gap-8'>
+                    <div className='flex gap-8 flex-wrap'>
                         <label className='flex flex-col text-left w-28'>Code
                             <select type="text" className='input-border ' >
                                 {/* <option>shbhj</option> */}
@@ -115,13 +107,13 @@ function Landing({ accessToken, isvalid }) {
 
             </section>
             <section className='flex flex-col items-center gap-3 w-full text-center bg-[#f0f0f0]  text-[#000] '>
-                <h2 className=' font-bold text-5xl p-10 px-20 leading-snug'>Revolutionizing Business Marketing</h2>
-                <div className='max-w-xl'>Revolutionize your business marketing strategies with Alt WhatsApp! Seamlessly connect with your audience, streamline communication, and unlock new possibilities for growth. Elevate your brand's presence and engagement on WhatsApp like never before.
+                <h2 className=' font-bold text-5xl p-10 px-20 leading-snug max-md:p-1 max-md:text-3xl max-sm:text-lg'>Revolutionizing Business Marketing</h2>
+                <div className='max-w-xl max-sm:text-xs px-3'>Revolutionize your business marketing strategies with Alt WhatsApp! Seamlessly connect with your audience, streamline communication, and unlock new possibilities for growth. Elevate your brand's presence and engagement on WhatsApp like never before.
                 </div>
             </section>
-            <section className='flex flex-col justify-center items-center gap-7 w-full text-center bg-[#afd5be] py-10 px-28'>
-                <h2 className=' font-bold text-5xl p-14 px-20 leading-snug  text-[#221231]'>Plan and Pricing</h2>
-                <div className='flex gap-4 '>
+            <section className='flex flex-col justify-center items-center gap-7 w-full text-center bg-[#afd5be] py-10 px-28 max-sm:px-4'>
+                <h2 className=' font-bold text-5xl p-14 px-20 leading-snug  text-[#221231] max-md:p-1 max-md:text-3xl max-sm:text-lg'>Plan and Pricing</h2>
+                <div className='flex justify-evenly gap-4 max-sm:text-xs max-sm:gap-4'>
                     <div>
                         <p>
                             Plan 1
